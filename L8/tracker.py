14fc903195f28a6f4d9d8a04d949d7c8fbbb2665
khengkok <khengkok@gmail.com>
2025-10-20 23:31:37 +0800

    added

--- L8/tracker.py
+++ L8/tracker.py
@@ -1,26 +1,32 @@
 import cv2
-
 from ultralytics import YOLO
 
-# Load the YOLO11 model
-model = YOLO("yolov8m.pt")
-model = YOLO("/Users/markk/Dev/git/iti121-2025s2/L6/weights/best.pt")
-
 # Open the video file
-video_path = "/Users/markk/Downloads/fish.mp4"
-video_path = "/Users/markk/Library/CloudStorage/OneDrive-Personal/Desktop/CET/SDAAI/2025S2/ITI121/L6a/goldfish2.m4v.mp4"
-cap = cv2.VideoCapture(video_path)
+cap = cv2.VideoCapture("samples/goldfish_2m.mp4")
+if not cap.isOpened():
+    print(f"Error: Could not open video file")
+    exit(-1)
+    
+# Get video properties
+width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+fps = int(cap.get(cv2.CAP_PROP_FPS))
+total_frames = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
 
-# Loop through the video frames
+print(f"Video properties: {width}x{height}, {fps} FPS, {total_frames} frames")
+
+# load the gold fish model 
+model = YOLO("samples/goldfish.pt")
+
+#Loop through the video frames
 while cap.isOpened():
     # Read a frame from the video
     success, frame = cap.read()
 
     if success:
-        # Run YOLO11 tracking on the frame, persisting tracks between frames
-        results = model.track(frame, persist=True)
+        # Run YOLO11 tracking on the frame, using tracker configuration from custom_tracker.yaml
+        results = model.track(frame, tracker="samples/custom_tracker.yaml", conf=0.7)
         
-
         # Visualize the results on the frame
         annotated_frame = results[0].plot()
 
