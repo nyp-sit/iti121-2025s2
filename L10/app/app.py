6053bf39b21dc614b81e1ddcf945559e6b0a9334
khengkok <khengkok@gmail.com>
2025-10-29 10:17:16 +0800

    added

---
+++ L10/app/app.py
@@ -0,0 +1,70 @@
+#------------------------------------------------------------------------------
+# To run your web server, open up your terminal / command prompt
+# and type:
+#    cd <path to this file>
+#    python practical-03c-deployment.py
+#
+#------------------------------------------------------------------------------
+
+from flask import Flask, flash, request, redirect, url_for, Response
+import requests
+import os
+import json
+import torch
+import numpy as np
+
+# Configure our application 
+#
+model_dir = 'model'
+
+# Initialize our Flask app.
+# NOTE: Flask is used to host our app on a web server, so that
+# we can call its functions over HTTP/HTTPS.
+#
+#app = Flask(__name__)
+labels = ["JUMPING", "JUMPING_JACKS", "BOXING", "WAVING_2HANDS", "WAVING_1HAND", "CLAPPING_HANDS"]
+
+app = Flask(__name__,
+            static_url_path='', 
+            static_folder='static',
+            template_folder='templates')
+
+if torch.cuda.is_available():
+    device = 'cuda'
+elif torch.mps.is_available():
+    device = 'mps'
+else:
+    device = 'cpu'
+    
+model_name = "activity_model.pth"
+model_path = os.path.join(model_dir, model_name)
+model = torch.load('activity_model.pth', weights_only=False).to(device)
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    print('receiving keypoints')
+    json_data = request.get_json()
+    x_str = json_data['instances']
+    X = torch.tensor(np.array(x_str), dtype=torch.float32).to(device)
+    pred = model(X).detach(cpu().
+    print(pred[0])
+    index = np.argmax(pred[0])
+    if pred[0][index] < 0.6:
+        activity = "UNKNOWN"
+    else:
+        activity = labels[index]
+    return Response(activity)
+
+
+
+#------------------------------------------------------------------------------
+# This starts our web server.
+# Although we are running this on our local machine,
+# this can technically be hosted on any VM server in the cloud!
+#------------------------------------------------------------------------------
+if __name__ == "__main__":
+    # Only for debugging while developing
+
+    app.run(host="0.0.0.0", debug=True, port=80)
+
+
